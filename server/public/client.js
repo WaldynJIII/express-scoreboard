13a386c9be6fb9fc35aeff440715d6e2e6d1dc79
Leo Dasso <leo.dasso@gmail.com>
2019-02-07 17:48:31 -0600

    make addPlayer button

--- server/public/client.js
+++ server/public/client.js
@@ -1,7 +1,47 @@
+class Player {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+let players = [
+    new Player('Greg'),
+    new Player('Luke'),
+    new Player('Vader'),
+    new Player('Leia')
+];
+
 $(document).ready(onReady);
 
 // this is called when the document is fully loaded
 function onReady() {
-    console.log('jquery ready');
-    
+
+    // listen for 'add player' button clicks
+    $('#addPlayerButton').on('click', addPlayer);
+
 }
+
+// Uses the input field in the player creator div
+// to add a new player object
+function addPlayer() {
+
+    let playerInput = $('#playerInput');
+
+    // alert the user if no name was entered
+    if (playerInput.val() == '') {
+        window.alert('Please enter a player name!');
+        return;
+    }
+
+    // create a new player using the input field, add it to the list of players
+    let newPlayer = new Player(playerInput.val());
+    players.push(newPlayer);
+
+    // clear the input field
+    playerInput.val('');
+}
+
+function refreshPlayerDisplay() {
+    // TODO the things
+}
+

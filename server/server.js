a5da5b570fa51328790ab89567e93532901c5fd8
Leo Dasso <leo.dasso@gmail.com>
2019-02-07 16:26:20 -0600

    allow index to be accessed via server, and add some placeholder stuff in index

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ let express = require('express');
 // Create an express app instance & ref it with 'app'
 let app = express();
 
+// Listen for requests
 app.listen(PORT, () => {
     console.log('Running on port ' + PORT);
 })
 
+// Use the stuff in server/public as the default route '/' (or something like that)
+app.use(express.static('server/public'));
